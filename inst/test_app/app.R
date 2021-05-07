00fd3f386ce79bc3f3b5d58aa18fb4f89cbde34b
EdBoChen <83742343+EdBoChen@users.noreply.github.com>
2021-05-07 11:03:59 +0800

    Update app.R

--- inst/test_app/app.R
+++ inst/test_app/app.R
@@ -1,5 +1,5 @@
 #test shiny app
-icd_tbl<- readr::read_rds("inst/test_app/testdataset.rds")
+icd_tbl<- readr::read_rds("inst/test_app/testdataset2.rda")
 ui<- fluidPage(
   sidebarLayout(
     sidebarPanel(
